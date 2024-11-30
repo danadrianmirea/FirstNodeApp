008260c099b36fdf5d8e804d64e65f9210654a23
Dan Adrian Mirea <damirea84@gmail.com>
2024-11-30 23:51:29 +0200

    remove init.bat

--- init.bat
+++
@@ -1,7 +0,0 @@
-echo "# FirstNodeApp" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:danadrianmirea/FirstNodeApp.git
-git push -u origin main
